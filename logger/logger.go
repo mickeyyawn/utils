6a5424395756c19b30009186c7d85984984b5ff6
Mickey Yawn <mickeyyawn@gmail.com>
2024-02-25 14:42:45 -0500

    initial commits

---
+++ logger/logger.go
@@ -0,0 +1,11 @@
+package log
+
+import (
+    "github.com/rs/zerolog/log"
+)
+
+func Test() {
+    zerolog.SetGlobalLevel(zerolog.ErrorLevel)
+    log.Info().Msg("Info message")
+    log.Error().Msg("Error message")
+}
---
+++ test.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+    "github.com/mickeyyawn/utils/log"
+)
+
+func main() {
+    //log.Info().Msg("Hello from Zerolog global logger")
+}
